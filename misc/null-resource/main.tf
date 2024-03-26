6dae7d8b70c0fa196d38769523a5d4cf952cfd15
Hever Farber <hever.farber@gmail.com>
2024-03-26 12:05:29 +0200

    add-wrong-provider

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -6,3 +6,12 @@ variable "counter" {
 resource "null_resource" "null" {
   count = var.counter
 }
+
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "8.2.2"
+    }
+  }
+}
