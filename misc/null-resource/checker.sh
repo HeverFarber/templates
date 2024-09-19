02f60d22d3d2ceebb82ff271e3ba643a0f8d0c8f
Hever Farber <hever.farber@gmail.com>
2024-09-19 10:45:20 +0300

    Update checker.sh

--- misc/null-resource/checker.sh
+++ misc/null-resource/checker.sh
@@ -5,6 +5,5 @@ EXPECTED_VALUE="secret1"
 
 # Check if ENV_VAR matches the expected value
 if [ "$SECRET" != "$EXPECTED_VALUE" ]; then
-  echo "Error: ENV_VAR is not equal to '$EXPECTED_VALUE'."
-  exit 1
+  echo "Error: SECRET is not equal to '$EXPECTED_VALUE'."
 fi
